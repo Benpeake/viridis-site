afb6030423a0d7174aaf08b2b4e33722e4c65ae8
Benpeake <dearbenp@gmail.com>
2023-11-20 14:39:45 +0000

    basic accordion logic and styles

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// accordion logic
+const accordionItems = document.querySelectorAll('.accordion-section');
+
+accordionItems.forEach(item => {
+    const expandIcon = item.querySelector('.expand-icon');
+    const accordContent = item.querySelector('.accordion-content');
+
+    expandIcon.addEventListener('click', function () {
+        accordContent.classList.toggle('active');
+        const newSrc = accordContent.classList.contains('active')
+        ? 'images/remove_icon.svg'
+        : 'images/expand_icon.svg';
+        expandIcon.src = newSrc;
+    });
+});
