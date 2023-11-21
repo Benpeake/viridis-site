23b94a54d360dbd6e488c2fe19537716c2e3febe
Benpeake <dearbenp@gmail.com>
2023-11-21 10:30:30 +0000

    adding about section

--- js/index.js
+++ js/index.js
@@ -37,7 +37,7 @@ function debounce(func, wait = 20, immediate = true) {
 function checkSlide() {
     fadeObjects.forEach((image) => {
         const slideInAt = window.scrollY + window.innerHeight;
-        const imageTop = image.offsetTop; // Use the exact offset top of the image
+        const imageTop = image.offsetTop; 
         const isTopShown = slideInAt > imageTop;
         const isNotScrolledPast = window.scrollY < imageTop;
 
@@ -50,5 +50,31 @@ function checkSlide() {
     });
 }
 
-
 document.addEventListener("scroll", debounce(checkSlide));
+
+//handle about section nav
+
+const aboutCopy = document.querySelector('.about-copy')
+const aboutCopyP2 = document.querySelector('.about-copy-p2')
+const aboutImg = document.querySelector('.about-img')
+const jemmaLink = document.querySelector('.jemma-nav-link')
+const teamLink = document.querySelector('.team-nav-link')
+
+jemmaLink.addEventListener('click', function(){
+    aboutCopy.textContent = ' Jemma has 10+ years in garden design and landscaping, during which time she has worked on a number of beautiful and prestigious sites including multiple gardens for the Duchy of Cornwall. Jemma loves being on site come rain or shine but also has a huge passion for the design process behind the scenes, and her passion for creativity and functionality has enabled  her to develop and craft her own original style that balances practicality and beauty.'
+    aboutCopyP2.textContent = ''
+    aboutImg.src = 'images/viridis_jemma.jpg'
+    aboutImg.alt = 'jemma in garden'
+    jemmaLink.classList.add('active-section-link')
+    teamLink.classList.remove('active-section-link')
+})
+
+teamLink.addEventListener('click', function(){
+    aboutCopy.textContent = 'Our fully-equipped diverse team consists of some of the most reliable and hardworking professionals in the field. We prioritise respecting our clients and their living space and always strive to deliver beautiful results that exceed expectation.'
+    aboutCopyP2.textContent = 'Our resources are derived from our hand-picked repertoire of high-quality contacts, developed through years of experience in the field. These include local nurseries for stunning assortments of plants, reliable skip provision to take care of rubble and garden waste, and further afield contacts for more unusual varieties of stone or other materials needed for building work.'
+    aboutImg.src = 'images/viridis_team.jpg'
+    aboutImg.alt = 'multiple gardeners gloves'
+    jemmaLink.classList.remove('active-section-link')
+    teamLink.classList.add('active-section-link')
+})
+
