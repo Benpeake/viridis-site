89da74be0d1694a50b34d0c0b72437987118d6e4
Benpeake <dearbenp@gmail.com>
2024-01-09 23:35:19 +0000

    animation tweaks

--- js/index.js
+++ js/index.js
@@ -48,25 +48,47 @@ const jemmaLink = document.querySelector(".jemma-nav-link");
 const teamLink = document.querySelector(".team-nav-link");
 
 jemmaLink.addEventListener("click", function () {
+  // Toggle active link instantly
+  jemmaLink.classList.add("active-section-link");
+  teamLink.classList.remove("active-section-link");
+
+  // Fade out existing content
+  gsap.to([aboutCopy, aboutCopyP2, aboutImg], { opacity: 0, duration: 0.6, ease: "sine.out", onComplete: updateContentJemma });
+});
+
+teamLink.addEventListener("click", function () {
+  // Toggle active link instantly
+  jemmaLink.classList.remove("active-section-link");
+  teamLink.classList.add("active-section-link");
+
+  // Fade out existing content
+  gsap.to([aboutCopy, aboutCopyP2, aboutImg], { opacity: 0, duration: 0.6, ease: "sine.out", onComplete: updateContentTeam });
+});
+
+function updateContentJemma() {
+  // Update content
   aboutCopy.textContent =
     " Jemma has 10+ years in garden design and landscaping, during which time she has worked on a number of beautiful and prestigious sites including multiple gardens for the Duchy of Cornwall. Jemma loves being on site come rain or shine but also has a huge passion for the design process behind the scenes, and her passion for creativity and functionality has enabled  her to develop and craft her own original style that balances practicality and beauty.";
   aboutCopyP2.textContent = "";
   aboutImg.src = "images/viridis_jemma.jpg";
   aboutImg.alt = "jemma in garden";
-  jemmaLink.classList.add("active-section-link");
-  teamLink.classList.remove("active-section-link");
-});
 
-teamLink.addEventListener("click", function () {
+  // Fade in new content
+  gsap.to([aboutCopy, aboutCopyP2, aboutImg], { opacity: 1, duration: 0.6, ease: "sine.in" });
+}
+
+function updateContentTeam() {
+  // Update content
   aboutCopy.textContent =
     "Our fully-equipped diverse team consists of some of the most reliable and hardworking professionals in the field. We prioritise respecting our clients and their living space and always strive to deliver beautiful results that exceed expectation.";
   aboutCopyP2.textContent =
     "Our resources are derived from our hand-picked repertoire of high-quality contacts, developed through years of experience in the field. These include local nurseries for stunning assortments of plants, reliable skip provision to take care of rubble and garden waste, and further afield contacts for more unusual varieties of stone or other materials needed for building work.";
   aboutImg.src = "images/viridis_team.jpg";
   aboutImg.alt = "multiple gardeners gloves";
-  jemmaLink.classList.remove("active-section-link");
-  teamLink.classList.add("active-section-link");
-});
+
+  // Fade in new content
+  gsap.to([aboutCopy, aboutCopyP2, aboutImg], { opacity: 1, duration: 0.6, ease: "sine.in" });
+}
 
 //cost calculator logic
 function quoteCalc(length, width) {
@@ -236,6 +258,14 @@ window.addEventListener("DOMContentLoaded", () => {
     stagger: { amount: 0.6 }
   });
 
+  // tl.from('.logo-letter', {
+  //   delay: 0.2,
+  //   opacity: 0,
+  //   duration: 0.4, 
+  //   ease: "sine.in", 
+  //   stagger: { amount: 0.4 }
+  // }, 0.4);
+
   tl.from(leafTop, {
     delay: 1.4,
     opacity: 0,
@@ -322,7 +352,7 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 0); 
 }
 
-  gsap.to('.logo-foot', {
+  gsap.from('.logo-foot', {
     scrollTrigger: {
       trigger: '.logo-foot',
       toggleActions: 'restart none none none',
@@ -330,9 +360,6 @@ window.addEventListener("DOMContentLoaded", () => {
     },
   });
 
-  
-  
-  
 
 
 })
