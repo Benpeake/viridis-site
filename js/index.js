c62b98ac6aff4caf5fa944a0a48e0e3959a22ddb
Benpeake <dearbenp@gmail.com>
2024-01-10 16:10:01 +0000

    adding revised copy accors site

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger)
 // accordion logic
 const accordionItems = document.querySelectorAll(".accordion-section");
 let openSection = null;
@@ -66,10 +67,29 @@ teamLink.addEventListener("click", function () {
 });
 
 function updateContentJemma() {
-  // Update content
-  aboutCopy.textContent =
-    " Jemma has 10+ years in garden design and landscaping, during which time she has worked on a number of beautiful and prestigious sites including multiple gardens for the Duchy of Cornwall. Jemma loves being on site come rain or shine but also has a huge passion for the design process behind the scenes, and her passion for creativity and functionality has enabled  her to develop and craft her own original style that balances practicality and beauty.";
-  aboutCopyP2.textContent = "";
+  // Clear existing content
+  aboutCopy.textContent = '';
+  aboutCopyP2.textContent = '';
+
+  // Create and append new elements
+  const smallPrint = document.createElement('span');
+  smallPrint.className = 'small-print';
+  smallPrint.appendChild(document.createTextNode('Jemma Smallman \u00A0 | \u00A0 Director '));
+  aboutCopy.appendChild(smallPrint);
+
+  const paragraphs = [
+    "Jemma has over 10 years of experience in horticulture. During this time, she has worked on beautiful and prestigious sites in and around Bath. This includes multiple gardens for the Duchy of Cornwall, listed city centre properties, and large estate gardens.",
+    "After training as a skilled gardener and designer in her previous role, working alongside another highly decorated garden designer, Jemma wanted to expand on her passion for the build side of projects. In 2020, she formed Viridis Landscapes.",
+    "Jemma loves being on site come rain or shine, but she also has a huge passion for the design process behind the scenes. Her enthusiasm for creativity and functionality has enabled her to develop and craft her own original style that balances practicality and beauty."
+  ];
+
+  paragraphs.forEach((paragraphText) => {
+    const paragraph = document.createElement('p');
+    paragraph.appendChild(document.createTextNode(paragraphText));
+    aboutCopy.appendChild(paragraph);
+  });
+
+  // Update image source and alt
   aboutImg.src = "images/viridis_jemma.jpg";
   aboutImg.alt = "jemma in garden";
 
@@ -77,10 +97,14 @@ function updateContentJemma() {
   gsap.to([aboutCopy, aboutCopyP2, aboutImg], { opacity: 1, duration: 0.2, ease: "sine.in" });
 }
 
+
+
+
+
 function updateContentTeam() {
   // Update content
   aboutCopy.textContent =
-    "Our fully-equipped diverse team consists of some of the most reliable and hardworking professionals in the field. We prioritise respecting our clients and their living space and always strive to deliver beautiful results that exceed expectation.";
+    "Our fully equipped diverse team consists of some of the most reliable and hardworking professionals in the field. We prioritise respecting our clients and their living space and always strive to deliver beautiful results that exceed expectation.";
   aboutCopyP2.textContent =
     "Our resources are derived from our hand-picked repertoire of high-quality contacts, developed through years of experience in the field. These include local nurseries for stunning assortments of plants, reliable skip provision to take care of rubble and garden waste, and further afield contacts for more unusual varieties of stone or other materials needed for building work.";
   aboutImg.src = "images/viridis_team.jpg";
@@ -96,16 +120,31 @@ function quoteCalc(length, width) {
   return roundedNum.toLocaleString();
 }
 
-document
-  .getElementById("quoteForm")
+document.getElementById("quoteForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    
+    const quoteResult = document.getElementById("quote-result");
     let length = document.getElementById("length").value;
     let width = document.getElementById("width").value;
     let estimate = quoteCalc(length, width);
-    document.getElementById("quote-result").textContent =
-      "Estimate: £" + estimate;
+    quoteResult.textContent = "£" + estimate;
+
+    // Split the text using SplitType
+    const splitTextResult = new SplitType(quoteResult, { types: 'words, chars' });
+
+    // GSAP animation on each character
+    gsap.from(splitTextResult.chars, {
+      delay: 0.2,
+      opacity: 0,
+      duration: 0.6,
+      ease: "sine.in",
+      stagger: { amount: 0.1 }
+    });
   });
+
+
+
 
 // Scroll link logic
 const scrollLinks = document.querySelectorAll(".scroll-link");
@@ -113,7 +152,7 @@ const scrollLinks = document.querySelectorAll(".scroll-link");
 function smoothScroll(e) {
   e.preventDefault();
 
-  const targetId = this.getAttribute("href");
+  const targetId = e.currentTarget.getAttribute("href");
   const targetSection = document.querySelector(targetId);
 
   if (!targetSection) {
@@ -151,12 +190,6 @@ scrollLinks.forEach((link) => {
   link.addEventListener("click", smoothScroll);
 });
 
-//on link-click show gallery images as well as use smooth scroll
-scrollLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    smoothScroll(e);
-  });
-});
 
 //form error handling...
 const textArea = document.getElementById("message");
@@ -242,8 +275,6 @@ mobileMenuLinks.forEach((link) => {
 //GSAP ANIMATION PLAY
 
 window.addEventListener("DOMContentLoaded", () => {
-
-  gsap.registerPlugin(ScrollTrigger)
 
   const splitText = new SplitType('#split-text');
   const tl = gsap.timeline();
